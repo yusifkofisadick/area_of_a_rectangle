c8c1e5b1ee7edc936d1e78b8068a2a7d5de12845
yusif kofi sadick <124945382+yusifkofisadick@users.noreply.github.com>
2024-11-09 16:33:55 -0800

    Add files via upload

---
+++ AOAT.py
@@ -0,0 +1,9 @@
+print()
+print("             Area of a rectangle calculator")
+print()
+length = int(input("Enter the length of the rectangle......?"))
+breadth = int(input("Enter the breadth of the rectangle......?"))
+
+area = length * breadth
+note = "Area of the rectangle is"
+print(note, area)
